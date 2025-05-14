e65f6ab9e42785c5b40f70a8625bb3f6168019b1
FCAraujo10 <cardoso.araujo@aluno.ifsp.edu.br>
2025-05-14 19:51:38 -0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double n1;
+	double n2;
+	
+	cout << "Digite o 1. numero: ";
+	cin >> n1;
+	cout << "Digite o 2. numero: ";
+	cin >> n2;
+	
+	if ( n1 > n2 )
+	{
+		cout << "O numero " << n1 << " eh maior que " << n2 << endl;
+	}
+	
+	if ( n2 > n1 )
+	{
+		cout << "O numero " << n2 << " eh maior que " << n1 << endl;
+	}
+	
+	if ( n1 == n2 ) 
+	{
+		cout << "Ambos dos Numeros sao iguais" << endl;
+	}
+	
+	return 0;
+}
